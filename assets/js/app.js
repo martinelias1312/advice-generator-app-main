058586fb10776efbd2e65ceb97bdeb32df4ff151
smradupan <martinelias630@gmail.com>
2022-04-19 14:16:33 +0200

    whole app.js code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,26 @@
 'use strict'
+
+const adviceId = document.querySelector(".adviceID");
+const adviceText = document.querySelector(".advice");
+const dice = document.querySelector(".dice-btn");
+
+dice.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const getAdvice = function () {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then(function (data) {
+          console.log(data)
+        const {
+          slip: { id },
+          slip: { advice },
+        } = data;
+        adviceId.innerHTML = id;
+        adviceText.innerHTML = advice;
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  getAdvice();
+});
