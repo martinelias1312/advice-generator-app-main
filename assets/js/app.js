0a421583dd6eb9cbb6e1f6d5a8e9cc0e43d60d9d
= <martinelias630@gmail.com>
2022-05-25 21:58:57 +0200

    no cache added to fetch for fix firefox lag issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,24 @@
-'use strict'
+"use strict";
 
 const adviceId = document.querySelector(".adviceID");
 const adviceText = document.querySelector(".advice");
 const dice = document.querySelector(".dice-btn");
 
 const getAdvice = function () {
-    fetch("https://api.adviceslip.com/advice")
-      .then((res) => res.json())
-      .then(function (data) {
-        const {
-          slip: { id },
-          slip: { advice },
-        } = data;
-        adviceId.innerHTML = id;
-        adviceText.innerHTML = advice;
-      })
-      .catch((err) => console.log(err.message));
+  fetch("https://api.adviceslip.com/advice", { cache: "no-cache" })
+    .then((res) => res.json())
+    .then(function (data) {
+      const {
+        slip: { id },
+        slip: { advice },
+      } = data;
+      adviceId.innerHTML = id;
+      adviceText.innerHTML = advice;
+    })
+    .catch((err) => console.log(err.message));
 };
 
-getAdvice()
+getAdvice();
 
 dice.addEventListener("click", () => {
   setTimeout(getAdvice, 2000);
